7dd3f2166cc8f21bcb036d959159c2526cc51ec8
Juan David <judaram98@gmail.com>
2023-09-07 11:47:16 -0600

    ✨feat:Creating javascript listeners

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const shortDescription = document.querySelector(".short-description");
   const fullDescription = document.querySelector(".full-description");
   const showMoreBtn = document.getElementById("show-more-btn");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburgerMenu.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
 
   showMoreBtn.addEventListener("click", () => {
     if (
